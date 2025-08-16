4f83ec6b95e9dba5ef367e719945f06df3463104
unknown <shifaterasulbd@gmail.com>
2025-08-16 08:33:00 -0700

    update

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Incourses</title>
+    <title>Incorese</title>
     @vite('resources/js/react-entry.jsx')
 </head>
 <body>
